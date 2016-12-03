5832cc936f4a0a3d9bed8ec4cdba99e49d642153
pankaj_dighe <pankajdighe77@gmail.com>
2016-12-03 02:38:53 -0800

    Added the changes

---
+++ app/main.js
@@ -0,0 +1,16 @@
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to resin.io dashboard logs
+var analogPin0 = new m.Aio(1); //setup access analog inpuput pin 0
+ //read the pin value as a float
+
+
+periodicActivity(); //call the periodicActivity function
+
+function periodicActivity()
+{
+
+	var analogValueFloat = analogPin0.readFloat();
+
+  console.log("Temperature Value "+analogValueFloat);
+  setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
+}
